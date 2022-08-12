95c70d4ae142e90ff97080366b3c928ad4529669
ronak.w <ronak.w@qburst.com>
2022-08-12 16:14:49 +0530

    added card info on click

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 function soundToggle() {
     toggle = document.getElementById("soundCassette").muted
     if(toggle) {
@@ -9,3 +14,28 @@ function soundToggle() {
         document.getElementById("soundCassette").muted = true;
     }
 }
+function toggleCard(id, cardIdArray) {
+    if(cardIdArray.includes(id)) {
+        console.log(id)
+        let displayImg = document.getElementById(id);
+        var sibling = document.getElementById(id).nextElementSibling;
+        displayImg.style.display = "none"
+        sibling.style.display = "inline-block"
+        setInterval(() => {
+            
+            displayImg.style.display = ""
+            sibling.style.display = "none"
+        }, 3000);
+    }
+}
+let cardCollection = document.getElementsByClassName("card-image")
+let cardCollectionArray = Array.prototype.slice.call(cardCollection)
+let cardIdArray = [];
+for (let i = 0;i < cardCollectionArray.length;i++) {
+    cardIdArray.push(cardCollectionArray[i].id)
+}
+console.log(cardIdArray)
+const onClick = (event) => toggleCard(event.srcElement.id, cardIdArray);
+window.addEventListener('click', onClick);
+
+
